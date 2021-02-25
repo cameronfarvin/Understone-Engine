de1534ecba88eaac1a87edfb240bd6052c1a6f59
assemblyDruid <cfarvin@gmail.com>
2021-02-25 08:41:05 -0500

    [ vulkan ] Reorganizing swap chain creation and recration

--- build/uBuilder.cpp
+++ build/uBuilder.cpp
@@ -923,24 +923,33 @@ class CompilerInvocationGenerator
         std::string           compilation_options_invocation = "";
         std::filesystem::path bin_directory(understone_root_dir + "/bin/" + project.executable_output_subfolder);
         std::string           executable_name = ToPosixPath(bin_directory.string()) + '/' + project.executable_name;
-        if (std::filesystem::exists(bin_directory) || std::filesystem::create_directory(bin_directory))
+
+        // Ensure that each folder along the provided path exists
+        std::string element_iter_string = "";
+        for (auto element_iter = bin_directory.begin(); element_iter != bin_directory.end(); ++element_iter)
         {
-            compilation_options_invocation += "-o ";
+            element_iter_string += element_iter->string();
+            std::filesystem::path element_iter_path(element_iter_string);
+            if (!std::filesystem::exists(element_iter_path) && !std::filesystem::create_directory(element_iter_path))
+            {
+                PrintLn("Unable to find or create the output directory specified by the project: " +
+                          project.project_name,
+                        OutputType::kError);
+                PrintLn("  Output folder: " + bin_directory.string(), OutputType::kError);
+                PrintLn("  Executable name: " + executable_name, OutputType::kError);
+                PrintLn("  Failed attempting to create folder: " + element_iter_string, OutputType::kError);
+
+                is_ok_ &= false;
+                return;
+            }
+        }
+
+        compilation_options_invocation += "-o ";
 #if _WIN32
-            compilation_options_invocation += executable_name + ".exe ";
+        compilation_options_invocation += executable_name + ".exe ";
 #else
-            compilation_options_invocation += executable_name + " ";
+        compilation_options_invocation += executable_name + " ";
 #endif
-        }
-        else
-        {
-            PrintLn("Unable to find or create the output directory specified by the project: " + project.project_name,
-                    OutputType::kError);
-            PrintLn("  Output folder: " + bin_directory.string(), OutputType::kError);
-            PrintLn("  Executable name: " + executable_name, OutputType::kError);
-            is_ok_ &= false;
-            return;
-        }
         compilation_options_invocation += link_parameters;
 
         if (is_ok_)
